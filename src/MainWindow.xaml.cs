7a4f93679fa78603ba60a4333f5eb82ffa7fd3f4
Happy <kolesnikjenya8@gmail.com>
2024-11-11 23:23:04 +0200

    addean image

---
+++ src/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Win32;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChangeImage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static BitmapImage _image;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files (*.png;*.jpg;)|*.png;*.jpg;";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(openFileDialog.FileName);
+                bitmap.EndInit();
+                _image = bitmap;
+                LoadedImage.Source = bitmap;
+            }
+        }
+
+        private async void ValueSlider_ValueChanged(object slender, RoutedEventArgs e)
+        {
+            await Task.Delay(1200);
+            double percents = ValueSlider.Value;
+
+            if (_image == null)
+                return;
+             
+            LoadedImage.Source = await Services.DivideImagePixelService(_image, percents);
+            SliderValueText.Text = $"Value: {ValueSlider.Value}";
+        }
+
+        private async void SaveImage_Click(object slender, RoutedEventArgs e)
+        {
+            if (LoadedImage.Source == null) 
+                return;
+
+            BitmapSource bitmapSource = LoadedImage.Source as BitmapSource;
+
+            await Services.SaveImageWithDialog(bitmapSource);
+        }
+    }
+}
---
+++ src/Services.cs
@@ -0,0 +1,89 @@
+﻿
+using Microsoft.Win32;
+using System.Drawing;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace ChangeImage
+{
+    internal static class Services
+    {
+        public static async Task<WriteableBitmap> DivideImagePixelService(BitmapImage image, double percents)
+        {
+            WriteableBitmap writeableBitmap = new WriteableBitmap(image);
+            int totalPixels = writeableBitmap.PixelWidth * writeableBitmap.PixelHeight;
+            int pixelsToDelete = (int)(totalPixels * percents / 100);
+
+            Random rand = new Random();
+
+            writeableBitmap.Lock();
+
+            try
+            {
+                for (uint i = 0; i < pixelsToDelete; i++)
+                {
+
+                    int x = rand.Next(writeableBitmap.PixelWidth);
+                    int y = rand.Next(writeableBitmap.PixelHeight);
+
+                    IntPtr buffer = writeableBitmap.BackBuffer + y * writeableBitmap.BackBufferStride + x * 4;
+
+                    int pixelValue = System.Runtime.InteropServices.Marshal.ReadInt32(buffer);
+
+                    if ((pixelValue & 0xFF000000) == 0)
+                    {
+                        i--;
+                        continue;
+                    }
+
+                    System.Runtime.InteropServices.Marshal.WriteInt32(buffer, 0x00000000);
+                }
+            }
+            finally
+            {
+                writeableBitmap.AddDirtyRect(new Int32Rect(0, 0, writeableBitmap.PixelWidth, writeableBitmap.PixelHeight));
+                writeableBitmap.Unlock();
+            }
+
+            return writeableBitmap;
+        }
+
+        public static async Task SaveImageWithDialog(BitmapSource image)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "PNG Image|*.png|JPEG Image|*.jpg|BMP Image|*.bmp",
+                Title = "Save Image As",
+                FileName = "MyImage"
+            };
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string filePath = saveFileDialog.FileName;
+                string extension = Path.GetExtension(filePath).ToLower();
+
+                BitmapEncoder encoder;
+
+                switch (extension)
+                {
+                    case ".jpg":
+                        encoder = new JpegBitmapEncoder();
+                        break;
+                    default:
+                        encoder = new PngBitmapEncoder();
+                        break;
+                }
+
+                encoder.Frames.Add(BitmapFrame.Create(image));
+
+                using (FileStream stream = new FileStream(filePath, FileMode.Create))
+                {
+                    encoder.Save(stream);
+                }
+
+                MessageBox.Show("The file was created!", "Saving file", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+    }
+}
